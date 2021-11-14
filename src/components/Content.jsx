9c4e108cddd7c95a4a8a5e64d97a38bd846b6d43
Ser Shor <shorser8@gmail.com>
2021-11-14 11:16:08 +0300

    add story

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,13 @@
 import Community from './Community';
 import News from './News';
+import Social from './Social';
 
 const Content = () => {
     return (
         <div className="content-wrapper">
             <Community/>
             <News/>
+            <Social/>
         </div>
     )
 }
---
+++ src/components/Social.jsx
@@ -0,0 +1,39 @@
+const Social = ()=>{
+    return (
+        <section className="social-wrapper">
+            <header className="stories-title">
+                <h2>Stories</h2>
+            </header>
+            <article className="stories">
+                <div className="friends-storie">
+                    <div className="author-wrapper">
+                        <figure className="author-avatar">
+                            <img className="avatar-user" src="https://static.tildacdn.com/tild3662-3765-4431-b934-393761356233/71317324_17535198697.png"
+                            alt="Elephant at sunset"/>
+                            <figcaption className="user-name">Alice Kramer</figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <div className="friends-storie">
+                    <div className="author-wrapper">
+                        <figure className="author-avatar">
+                            <img className="avatar-user" src="https://static.tildacdn.com/tild3662-3765-4431-b934-393761356233/71317324_17535198697.png"
+                            alt="Elephant at sunset"/>
+                            <figcaption className="user-name">Alice Kramer</figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <div className="friends-storie">
+                    <div className="author-wrapper">
+                        <figure className="author-avatar">
+                            <img className="avatar-user" src="https://static.tildacdn.com/tild3662-3765-4431-b934-393761356233/71317324_17535198697.png"
+                            alt="Elephant at sunset"/>
+                            <figcaption className="user-name">Alice Kramer</figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </article>
+        </section>
+    )
+}
+export default Social
